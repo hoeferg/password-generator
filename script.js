da6c0ef1d9d3d47b939c9ec9c9e7d5989716fe7a
hoeferg <111393796+hoeferg@users.noreply.github.com>
2022-09-15 20:00:30 -0700

    fix spelling

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-const lowerAlph = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
-const upperAlph = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
-const special = ['!','@','#','$','%','^','&','*'] 
-const num = ['0','1','2','3','4','5','6','7','8','9']
-const criteria = [lowerAlph, upperAlph, special, num]
+const lowerAlpha = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+const upperAlpha = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+const special = ['!','@','#','$','%','^','&','*'] ;
+const num = ['0','1','2','3','4','5','6','7','8','9'];
+const criteria = [lowerAlpha, upperAlpha, special, num];
 
 function generatePassword () {
-  length - prompt ('How many characters will your password be?')
-  if (length<8 || length>128)(
+  length = prompt ('How many characters will your password be?')
+  if (length < 8 || length > 128){
     alert ('number of characters must be between 8 and 128')
     generatePassword()
-}else{ 
+} else { 
 lowercase = confirm('Would you like to include lowercase?')
 uppercase = confirm('Would you like to include uppercase?')
 numeric = confirm('Would you like to include numeric values?')
@@ -21,17 +21,18 @@ specialChar = confirm('Would you like to include special character?')
 let criteriaSelect = [lowercase, uppercase, numeric, specialChar];
 let newCriteria = [];
 
-for (1=0; i < criteria.length; 1++);
+for (i=0; i < criteria.length; i++);{
   if (criteriaSelect[i]){
-    newCriteria= newCriteria.concat(criteri[1]);
+    newCriteria= newCriteria.concat(criteria[i]);
   }
 }
 
 console.log(newCriteria);
 
-passw = ""
+passW = ""
 for (i = 0; i < length; i++) {
-   newChar = newCriteria(Math.floor(Math.random[); * newCriteria.length)];
+   newChar = newCriteria[Math.floor(Math.random() * newCriteria.length)];
+   passW = passW.concat(newChar);
   }
   return passW;
 }
